a7c15af382ec389e1ef8bcc02f10bf53c52a4568
Vania Ferrari VF <vania.ferrari00@gmail.com>
2024-12-27 16:31:29 +0100

    initial personalisation

--- src/app/services/education.service.ts
+++ src/app/services/education.service.ts
@@ -9,32 +9,40 @@ export class EducationService {
 
   education = [
     {
-      studyTitle: "Human-Computer Interaction and Design",
-      university: "Université Paris-Saclay",
-      url: "https://www.universite-paris-saclay.fr/en/education/master/computer-science/m2-human-computer-interaction",
-      years: "September 2021",
-      graduationPhoto: "assets/images/graduation-photos/ups.jpg",
-    },
-    {
-      studyTitle: "Human-Computer Interaction and Design",
-      university: "Universidad Politécnica de Madrid",
-      url: "https://www.fi.upm.es/?pagina=2235",
-      years: "June 2020",
-      graduationPhoto: "assets/images/graduation-photos/upm.jpg",
-    },
-    {
-      studyTitle: "Bachelor Degree in Computer Science",
-      university: "Università degli studi di Trento",
-      url: "https://offertaformativa.unitn.it/it/l/informatica",
-      years: "June 2019",
-      graduationPhoto: "assets/images/graduation-photos/unitn.jpg",
-    },
-    {
-      studyTitle: "High School diploma in Computer Science",
-      university: "ITIS E. Fermi di Mantova",
-      url: "https://www.fermimn.edu.it/",
-      years: "June 2016",
-      graduationPhoto: "assets/images/graduation-photos/itis.jpg",
-    },
+          studyTitle: "ICT for internet and multimedia Engineering, Master Degree",
+          university: "Université Paris-Saclay",
+          url: "https://www.universite-paris-saclay.fr/en/education/master/computer-science/m2-human-computer-interaction",
+          years: "September 2021",
+          graduationPhoto: "assets/images/graduation-photos/unipd.jpg",
+      },
+
+      // {
+      //   studyTitle: "Human-Computer Interaction and Design",
+      //   university: "Université Paris-Saclay",
+      //   url: "https://www.universite-paris-saclay.fr/en/education/master/computer-science/m2-human-computer-interaction",
+      //   years: "September 2021",
+      //   graduationPhoto: "assets/images/graduation-photos/ups.jpg",
+      // },
+      // {
+      //   studyTitle: "Human-Computer Interaction and Design",
+      //   university: "Universidad Politécnica de Madrid",
+      //   url: "https://www.fi.upm.es/?pagina=2235",
+      //   years: "June 2020",
+      //   graduationPhoto: "assets/images/graduation-photos/upm.jpg",
+      // },
+      // {
+      //   studyTitle: "Bachelor Degree in Computer Science",
+      //   university: "Università degli studi di Trento",
+      //   url: "https://offertaformativa.unitn.it/it/l/informatica",
+      //   years: "June 2019",
+      //   graduationPhoto: "assets/images/graduation-photos/unitn.jpg",
+      // },
+      // {
+      //   studyTitle: "High School diploma in Computer Science",
+      //   university: "ITIS E. Fermi di Mantova",
+      //   url: "https://www.fermimn.edu.it/",
+      //   years: "June 2016",
+      //   graduationPhoto: "assets/images/graduation-photos/itis.jpg",
+      // },
   ]
 }
